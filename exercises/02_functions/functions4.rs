3e2091f04f83db34ea334aaa0fc36e08dfced4ae
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2023-10-25 01:02:32 +0000

    day one

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -8,18 +8,17 @@
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> f32 {
     if is_even(price) {
-        price - 10
+        ((price - 10) as f32) * 0.1
     } else {
-        price - 3
+        (price - 3) as f32
     }
 }
 
