0a9709ef35951e1a9a5d2ee51f9aecfcdc9c70f5
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2023-11-08 16:17:42 +0000

    day six take one

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
@@ -11,11 +10,14 @@ mod tests {
     fn simple_option() {
         let target = "rustlings";
         let optional_target = Some(target);
+        // let optional_target: Option<String> = None;
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
-        }
+        } else {
+            panic!("optional_target is empty");
+        };
     }
 
     #[test]
@@ -32,7 +34,7 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -12,9 +11,11 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
+    // let y: Option<(i32, i32)> = Some((1, 2));
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // Some(p) => println!("Co-ordinates are {},{} ", p.0, p.1),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
