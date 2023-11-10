8da16c35eb9f19e3a0545df6fa9f188459bbd44b
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2023-11-10 03:12:09 +0000

    day six take one

--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Rectangle {
     width: i32,
@@ -14,11 +13,12 @@ struct Rectangle {
 
 impl Rectangle {
     // Only change the test functions themselves
-    pub fn new(width: i32, height: i32) -> Self {
+    pub fn new(width: i32, height: i32) -> Result<Rectangle, String> {
         if width <= 0 || height <= 0 {
-            panic!("Rectangle width and height cannot be negative!")
+            Err(String::from("Rectangle width and height cannot be negative!"))
+        } else {
+            Ok(Rectangle {width, height})
         }
-        Rectangle {width, height}
     }
 }
 
@@ -29,20 +29,22 @@ mod tests {
     #[test]
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
-        let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        let rect = Rectangle::new(10, 20).unwrap();
+        assert_eq!(rect.width, 10); // check width
+        assert_eq!(rect.height, 20); // check height
     }
 
     #[test]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
+        assert!(_rect.is_err());
     }
 
     #[test]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
+        assert!(_rect.is_err());
     }
 }
